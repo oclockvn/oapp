ce479bba7a91d9392aae4692bc435f5fbc0f7993
oclockvn <oclockvn@gmail.com>
2017-05-05 12:36:17 +0700

    Add project files.

---
+++ oapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oapp.Startup))]
+namespace oapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
