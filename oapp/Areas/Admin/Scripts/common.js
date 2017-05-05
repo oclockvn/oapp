1d314a6b22938a3776c718f7fe90bebf040a0047
oclockvn <oclockvn@gmail.com>
2017-05-05 12:41:18 +0700

    add css

---
+++ oapp/Areas/Admin/Scripts/common.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    /**
+     * open sub menu
+     */
+    $(".menu").on("click", "> li > a", function (e) {
+        e.preventDefault();
+
+        var $self = $(this);
+        // var $li = $self.closest("li");
+        $self.toggleClass("active");
+
+        var $sub = $self.closest("li").find(".menu--sub");
+        if ($sub.length) {
+            $sub.slideToggle(300);
+        }
+    });
+
+    /**
+     * toggle sidebar menu
+     */
+    $(".toggle-menu-button").on("click", function (e) {
+        e.preventDefault();
+
+        $("#header").toggleClass("mini");
+        $("#sidebar").toggleClass("mini");
+    });
+
+})();
